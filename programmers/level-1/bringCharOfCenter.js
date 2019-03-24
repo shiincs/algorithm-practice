08a14d53bdd700d1725374c4d1f1b60c5d2fa0bd
shiincs <skywing27@gmail.com>
2019-03-24 11:29:10 +0900

    refac: edit some notations

--- programmers/level-1/bringCharOfCenter.js
+++ programmers/level-1/bringCharOfCenter.js
@@ -1,10 +1,9 @@
 // 가운데 글자 가져오기
 
 function solution(str) {
-  return str.length % 2 === 0
-    ? str.slice(str.length / 2 - 1, str.length / 2 + 1)
-    : // str.slice(Math.floor(str.length/2), Math.floor(str.length/2)+1)
-      str.charAt(Math.floor(str.length / 2));
+  return str.length % 2 === 0 ?
+    str.slice(str.length / 2 - 1, str.length / 2 + 1) : // str.slice(Math.floor(str.length/2), Math.floor(str.length/2)+1)
+    str.charAt(Math.floor(str.length / 2));
 }
 
 solution('abcde');
